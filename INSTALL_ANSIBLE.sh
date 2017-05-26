d82279dea7a6c092127ca5f84b22c5bdade5b294
vkurguzov1pt <kurguzov@1pt.com>
2017-05-26 19:11:38 +0300

    Update INSTALL_ANSIBLE.sh

--- INSTALL_ANSIBLE.sh
+++ INSTALL_ANSIBLE.sh
@@ -1,2 +1,10 @@
+sudo apt-get install build-essential autoconf libtool pkg-config python-opengl \
+python-imaging python-pyrex python-pyside.qtopengl idle-python2.7 qt4-dev-tools \
+qt4-designer libqtgui4 libqtcore4 libqt4-xml libqt4-test \
+libqt4-script libqt4-network libqt4-dbus python-qt4 python-qt4-gl \
+libgle3 python-dev libssl-dev -y
+
+# Need to rework what is above
+
 sudo apt-get install python-pip -y
-sudo pip install ansible -y
+sudo pip install ansible
